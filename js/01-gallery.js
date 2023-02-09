120b14f40b45e4b165bd1a4f870ec4e2883acc5c
Olena Savchenko <elena.volobuieva@gmail.com>
2023-02-09 23:42:51 +0300

    create ImgGallery markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+	//посилання на контейнер <div class="gallery"></div>
+const galleryBox = document.querySelector('.gallery');
+
+galleryBox.insertAdjacentHTML('beforeend', createImgGalleryMarkup(galleryItems));
+
+function createImgGalleryMarkup(images) { 
+	return images.map(({ original, preview, description }) => {
+		return `<div class="gallery__item">
+		<a class="gallery__link" href="${original}">
+		  <img
+			class="gallery__image"
+			src="${preview}	"
+			data-source="${original}"
+			alt="${description}"
+		  />
+		</a>
+	  </div>`;
+	}).join('');
+
+}
+
